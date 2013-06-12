afcfa7ef8588b57283173c30c91ee1827bf77034
daishihmr <daishi.hmr@gmail.com>
2013-06-13 02:37:45 +0900

    image

--- src/Player.js
+++ src/Player.js
@@ -1,6 +1,5 @@
 gls2.Player = tm.createClass({
     superClass: tm.app.Sprite,
-    keyboard: null,
     roll: 0,
     speed: 3,
     controllable: true,
@@ -8,44 +7,51 @@ gls2.Player = tm.createClass({
     init: function() {
         this.superInit("tex1", 64, 64);
         this.setPosition(SC_W * 0.5, SC_H + 32).setFrameIndex(3 + this.roll);
-        this.scaleX = this.scaleY = 0.75;
+        this.scaleX = this.scaleY = 0.5;
 
-        this.hitCircle = tm.app.Sprite("tex0", 64, 64).addChildTo(this)
+        this._createHitCircle();
+    },
+    _createHitCircle: function() {
+        this.hitCircle = tm.app.Sprite("tex0", 64, 64).addChildTo(this);
         this.hitCircle.setFrameIndex(7);
-        this.hitCircle.scaleX = this.hitCircle.scaleY = 0.2;
+        this.hitCircle.scale.set(0.2, 0.2);
         this.hitCircle.blendMode = "lighter";
         this.hitCircle.update = function(app) {
-            this.scaleX = this.scaleY = 0.15 + Math.sin(app.frame * 0.2) * 0.05;
+            var s = 0.15 + Math.sin(app.frame * 0.2) * 0.05;
+            this.scale.set(s, s);
         };
     },
-    update: function() {
+    update: function(app) {
         if (this.controllable) {
-            if (this.keyboard.getKey("up")) {
-                this.y -= this.speed;
-            } else if (this.keyboard.getKey("down")) {
-                this.y += this.speed;
-            }
-            if (this.keyboard.getKey("left")) {
+            var kb = app.keyboard;
+            if (kb.getKey("left")) {
                 this.x -= this.speed;
-                this.roll = Math.clamp(this.roll - 0.1, -3, 3);
-            } else if (this.keyboard.getKey("right")) {
+            } else if (kb.getKey("right")) {
                 this.x += this.speed;
-                this.roll = Math.clamp(this.roll + 0.1, -3, 3);
-            } else {
-                if (this.roll < 0) {
-                    this.roll = Math.clamp(this.roll + 0.1, -3, 3);
-                } else if (this.roll > 0){
-                    this.roll = Math.clamp(this.roll - 0.1, -3, 3);
-                }
             }
-        } else {
-            if (this.roll < 0) {
-                this.roll = Math.clamp(this.roll + 0.1, -3, 3);
-            } else if (this.roll > 0){
-                this.roll = Math.clamp(this.roll - 0.1, -3, 3);
+            if (kb.getKey("up")) {
+                this.y -= this.speed;
+            } else if (kb.getKey("down")) {
+                this.y += this.speed;
             }
         }
 
+        this._calcRoll(app);
+    },
+    _calcRoll: function(app) {
+        var inputLeft = this.controllable && app.keyboard.getKey("left");
+        var inputRight = this.controllable && app.keyboard.getKey("right");
+        if (inputLeft) {
+            this.roll = Math.clamp(this.roll - 0.1, -3, 3);
+        } else if (inputRight) {
+            this.roll = Math.clamp(this.roll + 0.1, -3, 3);
+        } else {
+            if (this.roll < 0) {
+                this.roll = Math.clamp(this.roll + 0.1, -3, 3);
+            } else if (0 < this.roll){
+                this.roll = Math.clamp(this.roll - 0.1, -3, 3);
+            }
+        }
         this.setFrameIndex(3 + ~~this.roll);
     }
 });
