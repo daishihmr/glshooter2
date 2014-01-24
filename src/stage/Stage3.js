161eb0fe63204eff38ac14288886d5f2f58d724a
minimo <minimo.mobile@gmail.com>
2014-01-24 13:18:59 +0900

    Stage3 update

--- src/stage/Stage3.js
+++ src/stage/Stage3.js
@@ -16,14 +16,18 @@ gls2.Stage3 = tm.createClass(
     init: function(gameScene) {
         this.superInit(gameScene);
 
+        //start
         this.seq.add(  0, function() {
             gls2.playBgm("bgm3", true),
             this.gameScene.ground.direction = Math.PI*0.5;
-            this.gameScene.ground.speed = 3;
+            this.gameScene.ground.speed = 2;
             this.gameScene.ground.tweener.clear().to({speed:5}, 4000, "easeInOutQuad");
         });
+        
+        //test
+        this.seq.add(100, "miyuki_y1");
 
-
+        //0%
         this.seq.add(150, "nao2-center");
         this.seq.add( 60, "nao2-left");
         this.seq.add( 60, "nao2-right");
@@ -37,9 +41,10 @@ gls2.Stage3 = tm.createClass(
         }
 
         this.seq.add( 60, function() {
-            this.gameScene.ground.tweener.clear().to({speed:3}, 1000, "easeInOutQuad");
+            this.gameScene.ground.tweener.clear().to({speed:7}, 1000, "easeInOutQuad");
         });
 
+        //10%
         this.seq.add(120, "akane-center");
         this.seq.add( 60, "nao2-center");
         this.seq.add( 60, "nao2-center");
@@ -59,14 +64,14 @@ gls2.Stage3 = tm.createClass(
         this.seq.add(120, "reika1-right");
         this.seq.add(180, "reika1-right");
 
-
+        //20%
         this.seq.add(120, function() {
             this.gameScene.ground.tweener
                 .clear()
-                .to({speed:5}, 2000, "easeInOutQuad")
-                .to({direction:Math.PI/180*90}, 3000, "easeInOutQuad")
+                .to({speed:6,direction:-Math.PI/180*90}, 3000, "easeInOutQuad")
+//                .to({direction:-Math.PI/180*90}, 3000, "easeInOutQuad")
         });
-
+  
         for (var i = 0; i < 3; i++) {
             this.seq.add( 60, "nao2-center");
             this.seq.add( 60, "nao2-left");
@@ -82,9 +87,13 @@ gls2.Stage3 = tm.createClass(
                 .to({speed:4}, 2000, "easeInOutQuad")
                 .to({direction:Math.PI*0.5}, 2000, "easeInOutQuad")
         });
+        
+        //30%
+        //40%
+        //50%
 
-        //中ボス
-        this.seq.add(600, "higashi", true);
+        //60%
+        this.seq.add(600, "higashi", true); //中ボス
 
         this.seq.add(300, "nao1-left");
         for (var i = 0; i < 8; i++) {
@@ -92,13 +101,16 @@ gls2.Stage3 = tm.createClass(
             this.seq.add( 60, "nao1-right");
             this.seq.add( 60, "nao1-left");
         }
+        //70%
 
+        //80%        
         this.seq.add(100, "alice");
         for (var i = 0; i < 8; i++) {
             this.seq.add( 60, "reika1-left");
             this.seq.add( 60, "reika1-right");
         }
 
+        //100%
         this.seq.add(600, function() {
             this.alartWarning(function() {
                 gls2.playBgm("bgmBoss", true);
